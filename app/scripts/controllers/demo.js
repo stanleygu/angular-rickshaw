f67c7497c8923c3f72eda6b3eabb82ca7cf2c296
Stanley Gu <stanleygu@gmail.com>
2013-05-28 23:12:38 -0400

    Controllers can provide graph data

---
+++ app/scripts/controllers/demo.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('angularRickshawApp').controller('DemoCtrl', function($scope) {
+
+    $scope.sampleData = [{
+        x: 0,
+        y: 40
+    }, {
+        x: 1,
+        y: 49
+    }, {
+        x: 2,
+        y: 38
+    }, {
+        x: 3,
+        y: 30
+    }, {
+        x: 4,
+        y: 32
+    }];
+});
